83396062fd918c8e8d02ea299a02f4383594f15f
Geetha V K <76592991+geethavani108@users.noreply.github.com>
2024-12-19 13:53:38 -0800

    Update register_model.py

--- register_model.py
+++ register_model.py
@@ -1,7 +1,25 @@
+# authenticate using Azurecli
 from azure.ai.ml.entities import Model
 from azure.ai.ml.constants import AssetTypes
-from config import ml_client
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+from azure.identity import AzureCliCredential
+#credential = DefaultAzureCredential()
+credential = AzureCliCredential()
+subscription_id= "your subscription",
+resource_group= "gv-dp100-l744c846c2ac2425e86",
+workspace_name= "mlw-dp100-l744c846c2ac2425e86"
 
+ml_client = MLClient(
+    credential=credential,
+    subscription_id=subscription_id,
+    resource_group=resource_group,
+    workspace_name=workspace_name
+)
+
+print("MLClient initialized successfully in configuration")
+# Initialize MLClient
+ml_client = MLClient.from_config(credential=credential)
 # Define the model name and path
 model_name = 'heart-prediction-pickle-model'
 
